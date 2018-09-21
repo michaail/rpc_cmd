ecd0e93f63cab975d8254f39cea12cf85a482131
klos <galeo09@gmail.com>
2018-09-21 02:33:21 +0200

    final working fine

--- rpc_cmd_server.c
+++ rpc_cmd_server.c
@@ -13,7 +13,7 @@
 #include <stdio.h>
 #include <errno.h>
 
-char * readBuff(FILE * stream)
+char * readBuff(FILE* stream)
 {
     char buff[10];
     size_t cur_len = 0;
@@ -44,17 +44,13 @@ char * readBuff(FILE * stream)
 
 cmdCallback * rcmd_1_svc(cmdParams *argp, struct svc_req *rqstp)
 {
-	static cmdCallback result;
-
-    // Length of whole command + arguments to call
-    int wholeLen = strlen(argp->command);
+    int wholeLen = strlen(argp->command);   // Length of whole: command + arguments 
     for(int i = 0; i < argp->arguments.arguments_len; i++)
     {
         wholeLen = wholeLen + strlen(argp->arguments.arguments_val[i]) + 1;
     }
-    //printf("debug: wholeLen passed [%d]\n", wholeLen);
     
-    // Prepare whole cmd with arguments in one string
+    // Combine command and arguments into one string
     char* cmd_ = malloc(wholeLen + 1);
     strcpy(cmd_, argp->command);
     strcat(cmd_, " ");
@@ -64,213 +60,69 @@ cmdCallback * rcmd_1_svc(cmdParams *argp, struct svc_req *rqstp)
         strcat(cmd_, " ");
     }
     
-    //printf("%s\n", cmd_);
-    
-    
-    // command print Debug only
-	//printf("%s %d\n", (char *)argp->command, argp->arguments.arguments_len);
-	//printf("%s\n", argp->pipeData);
-
+    printf("%s\n", cmd_);
+ 
     // fd[0]: read end | fd[1]: write end
-    // int stdIn[2];   // stdin    descriptors
-    // int stdOut[2];  // stdout   descriptors
-    // int stdErr[2];  // stderr   descriptors
+    int stdInput[2];    // stdin    descriptors
+    int stdOutput[2];   // stdout   descriptors
+    int stdError[2];    // stderr   descriptors
 
-    int inputf[2], outputf[2], errorf[2];
-    pipe(inputf);
-    pipe(outputf);
-    pipe(errorf);
+    pipe(stdInput);
+    pipe(stdOutput);
+    pipe(stdError);
 
-    int pid;
-    if((pid=fork())==0)
-    {
+    static cmdCallback result;  // command result to be pass to client
 
-        //     // if (dup2(stdIn[0], STDIN_FILENO) == -1) {
-    //     //     perror("dup2");
-    //     // }
-    //     // if (dup2(stdOut[1], STDOUT_FILENO) == -1) {
-    //     //     perror("dup2");
-    //     // }
-    //     // if (dup2(stdErr[1], STDERR_FILENO) == -1) {
-    //     //     perror("dup2");
-    //     // }
-        
-        if (dup2(inputf[0], STDIN_FILENO)) 
-        {
-            /* code */
-        }
+    int cpid;
+    if((cpid=fork())==0)
+    {     
+        // duplikacja deskryptorów plików
+        dup2(stdInput[0], STDIN_FILENO);
+        dup2(stdOutput[1], STDOUT_FILENO);
+        dup2(stdError[1], STDERR_FILENO);
 
-        // if (/* condition */) {
-        //     /* code */
-        // }
-        
-        
-        dup2(inputf[0], STDIN_FILENO);
-        dup2(outputf[1], STDOUT_FILENO);
-        dup2(errorf[1], STDERR_FILENO);
+        close(stdInput[0]);
+		close(stdOutput[1]);
+		close(stdError[1]);
+		close(stdInput[1]);
+		close(stdOutput[0]);
+		close(stdError[0]);
 
-        close(inputf[0]);
-		close(outputf[1]);
-		close(errorf[1]);
-		close(inputf[1]);
-		close(outputf[0]);
-		close(errorf[0]);
-
-        result.stat = WEXITSTATUS(system(cmd_));
+        result.stat = WEXITSTATUS(system(cmd_));    // wywołanie komendy
         exit(result.stat);
 
     }
     else
     {
-        close(inputf[0]);
-		close(outputf[1]);
-		close(errorf[1]);
+        close(stdInput[0]);
+		close(stdOutput[1]);
+		close(stdError[1]);
         // wpisanie na wyjście stdin strumienia z potoku
-		write(inputf[1], argp->pipeData, strlen(argp->pipeData)+1);
-		close(inputf[1]);
+		write(stdInput[1], argp->pipeData, strlen(argp->pipeData)+1);
+		close(stdInput[1]);
 
         int status;
-		waitpid(pid, &status, 0);
+		waitpid(cpid, &status, 0);
 		result.stat = WEXITSTATUS(status);
 
         // ustawienie kanałów na nieblokujące
-		fcntl(outputf[0], F_SETFL, O_NONBLOCK);
-		fcntl(errorf[0], F_SETFL, O_NONBLOCK);
-		result.stdout = (char*)malloc(sizeof(char)*1024);
-		result.stderr = (char*)malloc(sizeof(char)*1024);
-		
-        int count=0, stdoutBufSize=0, stderrBufSize=0;
-		while((count = read(outputf[0], result.stdout+stdoutBufSize, 1024))>0){
-			stdoutBufSize += count;
-			result.stdout = (char*)realloc(result.stdout, sizeof(char)*(stdoutBufSize+1024));
-		}
-		while((count = read(errorf[0], result.stderr+stderrBufSize, 1024))>0){
-			stderrBufSize += count;
-			result.stderr = (char*)realloc(result.stderr, sizeof(char)*(stderrBufSize+1024));
-		}
-		result.stdout[stdoutBufSize] = 0;
-		result.stderr[stderrBufSize] = 0;
+		fcntl(stdOutput[0], F_SETFL, O_NONBLOCK);
+		fcntl(stdError[0], F_SETFL, O_NONBLOCK);
+        
+        FILE* stdOutputFile = fdopen(stdOutput[0], "r");
+        result.stdout = readBuff(stdOutputFile);    // odczytanie strumienia
+        fclose(stdOutputFile);
+        if (!result.stdout) {   // problem przy pustym wskaźniku
+            result.stdout = (char*)malloc(sizeof(char)* 1);
+        }
+        
+        FILE* stdErrorFile = fdopen(stdError[0], "r");
+        result.stderr = readBuff(stdErrorFile);     // odczytanie strumienia
+        fclose(stdErrorFile);
+        if (!result.stderr) {   // problem przy pustym wskaźniku
+            result.stderr = (char*)malloc(sizeof(char)* 1);
+        }
 
         return &result;
     }
-
-    // Create pipes for stdS (unidirectional data channels)
-    
-
-    // Tu się problem zaczyna
-    // if (pipe(stdIn) == -1) {
-    //     perror("pipe");
-    // }
-    
-    // //pipe(stdIn);
-    // pipe(stdOut);
-    // pipe(stdErr);
-    
-
-    // int cpid = fork();
-    // if(cpid == 0)   // Child process
-    // {
-        
-    //     // if (dup2(stdIn[0], STDIN_FILENO) == -1) {
-    //     //     perror("dup2");
-    //     // }
-    //     // if (dup2(stdOut[1], STDOUT_FILENO) == -1) {
-    //     //     perror("dup2");
-    //     // }
-    //     // if (dup2(stdErr[1], STDERR_FILENO) == -1) {
-    //     //     perror("dup2");
-    //     // }
-        
-    //     //dup2(stdIn[0], STDIN_FILENO);
-    //     //dup2(stdOut[1], STDOUT_FILENO);
-    //     //dup2(stdErr[1], STDERR_FILENO);
-
-    //     //printf("forked duped\n");
-    //     close(stdIn[0]);
-    //     close(stdOut[1]);
-    //     close(stdErr[1]);
-
-    //     // close(stdIn[1]);
-    //     // close(stdOut[0]);
-    //     // close(stdErr[0]);
-
-    //     //printf("debug fork\n");
-    //     // Cmd call in child process returns exit status
-    //     int status = WEXITSTATUS(system(cmd_));
-    //     //printf("%d\n", status);
-    //     exit(status);
-    // }
-    // else            // Parent process
-    // {
-    //     // Sets Nonblocking mode for streams
-    //     close(stdIn[0]);
-    //     close(stdOut[1]);
-    //     close(stdErr[1]);
-    //     // write from buffer to stream
-    //     write(stdIn[1], argp->pipeData, strlen(argp->pipeData) + 1);
-    //     close(stdIn[1]);
-    //     //printf("to wait\n");
-
-    //     // Dotąd proces macierzysty działa OK
-    //     int sts;
-    //     waitpid(cpid, &sts, 0);             // Wait for child process to change state
-    //     result.stat = WEXITSTATUS(sts);     // returns exit status of child process
-
-    //     fcntl(stdOut[0], F_SETFL, O_NONBLOCK);
-    //     fcntl(stdErr[0], F_SETFL, O_NONBLOCK);
-
-    //     char *inputStdOut, *inputStdErr;
-
-    //     inputStdOut = readBuff(stdOut[0]);
-
-    //     result.stdout = inputStdOut;
-
-    //     inputStdErr = readBuff(stdErr[0]);
-
-    //     result.stderr = inputStdErr;
-
-    //     return &result;
-    // }
-
-	// FILE *ls = popen("ls", "r");		// wywołanie komendy
-	
-    // odczyt printu komendy komendy
-    // char buffer[10];
-	// char *input = 0;
-    // size_t cur_len = 0;
-    // size_t cur_max = 0;
-
-	// while (fgets(buffer, sizeof(buffer), ls) != 0)
-    // {
-    //     size_t buf_len = strlen(buffer);
-    //     if (cur_len + buf_len + 1 > cur_max)
-    //     {
-    //         size_t new_len = cur_max * 2 + 1;
-    //         if (buf_len + 1 > new_len)
-    //             new_len = buf_len + 1;
-    //         char *extra = realloc(input, new_len);
-    //         if (extra == 0)
-    //             break;
-    //         input = extra;
-    //         cur_max = new_len;
-    //     }
-    //     strcpy(input + cur_len, buffer);
-    //     cur_len += buf_len;
-    // }
-
-    // printf("%s\n", input);
-
-    // temp
-    // char * err;
-    // err = (char*)malloc(sizeof(char)* 1);
-
-    // result.stderr = err;
-    // result.stdout = input;
-	// result.stat = 0;
-
-	/*
-	 * insert server code here
-	 */
-
-	
 }
